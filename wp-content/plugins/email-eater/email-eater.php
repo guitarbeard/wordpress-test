eb5fff8a6f9987e090faa8258bb7a605d9077020
Alex Medina <alxdna@gmail.com>
2015-12-19 15:37:37 -0800

    adds email plugin folder and info

---
+++ wp-content/plugins/email-eater/email-eater.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Email Eater
+ * Description: This plugin collects emails.
+ * Version: 1.0.0
+ * Author: Alex Medina
+ * Author URI: http://www.alxdna.net
+ * License: MIT
+ */
