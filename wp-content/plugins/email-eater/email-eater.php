c179602cfafe010b3da9775a8feb4cd88e7edd3b
Alex Medina <alxdna@gmail.com>
2015-12-19 16:29:26 -0800

    adds basic form function

--- wp-content/plugins/email-eater/email-eater.php
+++ wp-content/plugins/email-eater/email-eater.php
@@ -7,3 +7,33 @@
  * Author URI: http://www.alxdna.net
  * License: MIT
  */
+
+ // enqueue and localise scripts
+ wp_enqueue_script( 'my-ajax-handle', plugin_dir_url( __FILE__ ) . 'js/email-eater.js', array( 'jquery' ) );
+ wp_localize_script( 'my-ajax-handle', 'the_ajax_script', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+ // THE AJAX ADD ACTIONS
+ add_action( 'wp_ajax_the_ajax_hook', 'the_action_function' );
+ add_action( 'wp_ajax_nopriv_the_ajax_hook', 'the_action_function' ); // need this to serve non logged in users
+ // THE FUNCTION
+ function the_action_function(){
+   /* this area is very simple but being serverside it affords the possibility of retreiving data from the server and passing it back to the javascript function */
+   $name = $_POST['email'];
+   echo"Hello World, " . $name;// this is passed back to the javascript function
+   die();// wordpress may print out a spurious zero without this - can be particularly bad if using json
+ }
+ function email_eater_funtion(){
+   $the_form = '
+    <form id="emailEaterForm">
+      <input id="emailInput" name="email" type="text" placeholder="email" />
+      <input name="action" type="hidden" value="the_ajax_hook" />
+      <button id="submitEmailButton" type="button" onClick="submit_me();">
+        Sign up
+      </button>
+    </form>
+    <div id="response_area">
+      This is where we\'ll get the response
+    </div>';
+   return $the_form;
+ }
+ add_shortcode("email_eater", "email_eater_funtion");
+ ?>
---
+++ wp-content/plugins/email-eater/js/email-eater.js
@@ -0,0 +1,6 @@
+function submit_me(){
+  jQuery.post(the_ajax_script.ajaxurl, jQuery("#emailEaterForm").serialize(),
+  function(response_from_the_action_function){
+    jQuery("#response_area").html(response_from_the_action_function);
+  });
+}
